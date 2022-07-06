377b3579db9399a95e07e783115cdc3b3796950e
MillieMuteti <milliemuteti@gmail.com>
2022-07-06 10:40:43 -0700

    Task 3 attempt

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,19 +1,33 @@
-#(int *array, int size, int (*cmp)(int))
+#include"3-calc.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * get_op_func - function finds function for operator sign
+ *
+ *@s: char * string
+ *
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
 {
-  int i;
 
-  if (array == NULL || cmp == NULL)
-    return (-1);
-  if (size <= 0)
-    return (-1);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-  for (i = 0; i < size; i++)
-    {
-      if (cmp(array[i]) != 0)
+	while (ops[i].op != NULL)
 	{
-	  return (i);
+		if (!strcmp(s, ops[i].op))
+			return (ops[i].f);
+		i++;
 	}
-    }
-
-  return (-1);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,55 +1,82 @@
-#include "3-calc.h"
+#include"3-calc.h"
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * op_add - adds two ints
- * @a: number 1
- * @b: number 2
- * Return: sum of the two numbers
- **/
+ * op_add - function adds 2 ints
+ *
+ *@a: int number
+ *@b: int number
+ *
+ * Return: int
+ */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - subtract two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of substraction
- **/
+ * op_sub - function subtracts 2 ints
+ *
+ *@a: int number
+ *@b: int number
+ *
+ * Return: int
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - multiply two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of multiplication
- **/
+ * op_mul - function multiplies 2 ints
+ *
+ *@a: int number
+ *@b: int number
+ *
+ * Return: int
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - divides two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of divison
- **/
+ * op_div - function divides 2 ints
+ *
+ *@a: int number
+ *@b: int number
+ *
+ * Return: int
+ */
+
 int op_div(int a, int b)
 {
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
+
 /**
- * op_mod - calculates mod of two ints
- * @a: number 1
- * @b: number 2
- * Return: mod of 1, 2
- **/
+ * op_mod - function moduluses 2 ints
+ *
+ *@a: int number
+ *@b: int number
+ *
+ * Return: int
+ */
+
 int op_mod(int a, int b)
 {
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
